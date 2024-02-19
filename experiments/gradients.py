48610744ce9a73401c590c93e966e977f36d9f35
christhetree <christhetree@gmail.com>
2024-02-19 16:01:29 +0000

    [cm] Adding trials to gradient vis

--- experiments/gradients.py
+++ experiments/gradients.py
@@ -21,40 +21,36 @@ log = logging.getLogger(__name__)
 log.setLevel(level=os.environ.get('LOGLEVEL', 'INFO'))
 
 
-def calc_distance_grad_matrix(dist_func: nn.Module,
-                              synth: ChirpTextureSynth,
-                              theta_density: Optional[T] = None,
-                              theta_slope: Optional[T] = None,
-                              n_density: int = 9,
-                              n_slope: int = 9,
-                              use_rand_seeds: bool = False,
-                              save_path: Optional[str] = None,
-                              grad_clip_val: Optional[float] = None,
-                              seed: int = 42) -> None:
-    # TODO(cm): control seed, separate plotting
-
-    if theta_density is None:
-        theta_density = tr.tensor(0.5)
-    if theta_slope is None:
-        theta_slope = tr.tensor(0.0)
+def calc_distance_grad_matrices(dist_func: nn.Module,
+                                synth: ChirpTextureSynth,
+                                theta_density: T,
+                                theta_slope: T,
+                                n_density: int = 9,
+                                n_slope: int = 9,
+                                use_rand_seeds: bool = False,
+                                grad_clip_val: Optional[float] = None,
+                                seed: int = 42,
+                                pbar: Optional[tqdm] = None) -> (T, T, T, T, T):
+    # TODO(cm): control meso seed
     seed = tr.tensor(seed)
     x = synth(theta_density, theta_slope, seed)
-    theta_density_indices = list(range(n_density))
-    theta_slope_indices = list(range(n_slope))
+
     theta_density_hats = tr.linspace(0.0, 1.0, n_density + 2, requires_grad=True)[1:-1]
     theta_slope_hats = tr.linspace(-1.0, 1.0, n_slope + 2, requires_grad=True)[1:-1]
     dist_rows = []
     density_grad_rows = []
     slope_grad_rows = []
-    for theta_density_hat in tqdm(theta_density_hats):
+
+    for theta_density_hat in theta_density_hats:
         dist_row = []
         density_grad_row = []
         slope_grad_row = []
-        for theta_slope_hat in tqdm(theta_slope_hats):
+        for theta_slope_hat in theta_slope_hats:
+            seed_hat = seed
             if use_rand_seeds:
                 # TODO(cm): make cleaner
-                seed = tr.randint(seed.item(), seed.item() + 999999, (1,))
-            x_hat = synth(theta_density_hat, theta_slope_hat, seed)
+                seed_hat = tr.randint(seed.item(), seed.item() + 999999, (1,))
+            x_hat = synth(theta_density_hat, theta_slope_hat, seed_hat)
             dist = dist_func(x_hat.view(1, 1, -1), x.view(1, 1, -1))
             dist = dist.squeeze()
             dist_row.append(dist.item())
@@ -63,45 +59,48 @@ def calc_distance_grad_matrix(dist_func: nn.Module,
                 dist, [theta_density_hat, theta_slope_hat])
             density_grad_row.append(density_grad.item())
             if slope_grad is None:
+                log.warning(f"slope_grad is None")
                 slope_grad_row.append(0.0)
             else:
                 slope_grad_row.append(slope_grad.item())
+
+            if pbar is not None:
+                pbar.update(1)
+
         dist_rows.append(dist_row)
         density_grad_rows.append(density_grad_row)
         slope_grad_rows.append(slope_grad_row)
 
     dist_matrix = tr.tensor(dist_rows)
     dgm = tr.tensor(density_grad_rows)
-    # dgm_mu = dgm.mean()
-    # dgm_std = dgm.std()
-    # log.info(f"dgm_mu={dgm_mu:.4f}, dgm_std={dgm_std:.4f}, max dgm={dgm.abs().max():.4f}")
-    # dgm = (dgm - dgm_mu) / dgm_std
-    # dgm = tr.clip(dgm, -3, 3)
     sgm = tr.tensor(slope_grad_rows)
-    # sgm_mu = sgm.mean()
-    # sgm_std = sgm.std()
-    # log.info(f"sgm_mu={sgm_mu:.4f}, sgm_std={sgm_std:.4f}, max sgm={sgm.abs().max():.4f}")
-    # sgm = (sgm - sgm_mu) / sgm_std
-    # sgm = tr.clip(sgm, -3, 3)
-    # log.info(f"dgm=\n{dgm}")
-    # log.info(f"sgm=\n{sgm}")
-
-    log.info(f"max  dgm={dgm.abs().max():.4f}")
-    log.info(f"mean dgm={dgm.abs().mean():.4f}")
-    log.info(f"max  sgm={sgm.abs().max():.4f}")
-    log.info(f"mean sgm={sgm.abs().mean():.4f}")
 
     if grad_clip_val is not None:
+        assert False  # Disable for now
+        # TODO(cm): this is different from default clipping which acts on the norm
         log.info(f"grad_clip_val={grad_clip_val:.2f}")
         dgm = tr.clip(dgm, -grad_clip_val, grad_clip_val)
         sgm = tr.clip(sgm, -grad_clip_val, grad_clip_val)
 
-    max_grad = max(dgm.abs().max(), sgm.abs().max())
-    log.info(f"max_grad={max_grad:.4f}")
-    dgm /= max_grad
-    sgm /= max_grad
+    return theta_density_hats, theta_slope_hats, dist_matrix, dgm, sgm
 
-    dist_matrix = dist_matrix / dist_matrix.abs().max()
+
+def plot_gradients(theta_density: T,
+                   theta_slope: T,
+                   dist_matrix: T,
+                   theta_density_hats: T,
+                   theta_slope_hats: T,
+                   dgm: T,
+                   sgm: T,
+                   title: Optional[str] = None,
+                   save_path: Optional[str] = None) -> None:
+    if title is None:
+        title = f"gradients\nθ density={theta_density:.2f}, θ slope={theta_slope:.2f}"
+
+    n_density = dist_matrix.size(0)
+    n_slope = dist_matrix.size(1)
+    theta_density_indices = list(range(n_density))
+    theta_slope_indices = list(range(n_slope))
 
     fontsize = 14
     ax = plt.gca()
@@ -127,11 +126,7 @@ def calc_distance_grad_matrix(dist_func: nn.Module,
               angles='xy',
               scale=8.0,
               scale_units="width")
-    ax.set_title(f"{dist_func.__class__.__name__}\n"
-                 f"θ density={theta_density:.2f}, "
-                 f"θ slope={theta_slope:.2f}, "
-                 f"{'meso' if use_rand_seeds else 'micro'}",
-                 fontsize=fontsize)
+    ax.set_title(title, fontsize=fontsize)
     plt.tight_layout()
 
     if save_path is not None:
@@ -159,6 +154,7 @@ if __name__ == "__main__":
     with open(config_path, 'r') as f:
         config = yaml.safe_load(f)
     synth = ChirpTextureSynth(**config["init_args"])
+    synth = synth.to(device)
 
     config_path = os.path.join(CONFIGS_DIR, "losses/scrapl.yml")
     with open(config_path, 'r') as f:
@@ -177,21 +173,26 @@ if __name__ == "__main__":
     # dist_func = scrapl_loss
     # dist_func = jtfst_loss
 
-    synth = synth.to(device)
     dist_func = dist_func.to(device)
 
-    use_rand_seeds = False
-    # use_rand_seeds = True
+    use_rand_seeds = False  # Micro
+    # use_rand_seeds = True  # Meso
+    theta_density = tr.tensor(0.5)
+    theta_slope = tr.tensor(0.5)
+    n_density = 3
+    n_slope = 7
+    n_trials = 2
 
-    n_density = 9
-    n_slope = 9
-    suffix = (f"d{n_density}s{n_slope}__J{jtfst_loss.jtfs.J}"
-              f"_Q{jtfst_loss.jtfs.Q[0]}"
-              f"_QQ{jtfst_loss.jtfs.Q[1]}"
-              f"_Jfr{jtfst_loss.jtfs.J_fr}"
-              f"_Qfr{jtfst_loss.jtfs.Q_fr[0]}"
-              f"_T{jtfst_loss.jtfs.T}"
-              f"_F{jtfst_loss.jtfs.F}")
+    if dist_func == jtfst_loss:
+        suffix = (f"d{n_density}s{n_slope}t{n_trials}__J{jtfst_loss.jtfs.J}"
+                  f"_Q{jtfst_loss.jtfs.Q[0]}"
+                  f"_QQ{jtfst_loss.jtfs.Q[1]}"
+                  f"_Jfr{jtfst_loss.jtfs.J_fr}"
+                  f"_Qfr{jtfst_loss.jtfs.Q_fr[0]}"
+                  f"_T{jtfst_loss.jtfs.T}"
+                  f"_F{jtfst_loss.jtfs.F}")
+    else:
+        suffix = f"d{n_density}s{n_slope}t{n_trials}"
 
     if use_rand_seeds:
         save_name = f"dist__{dist_func.__class__.__name__}__meso__{suffix}.png"
@@ -201,14 +202,63 @@ if __name__ == "__main__":
     log.info(f"save_name={save_name}")
 
     save_path = os.path.join(OUT_DIR, save_name)
-    calc_distance_grad_matrix(dist_func,
-                              synth,
-                              theta_density=tr.tensor(0.6),
-                              theta_slope=tr.tensor(0.0),
-                              n_density=n_density,
-                              n_slope=n_slope,
-                              use_rand_seeds=use_rand_seeds,
-                              save_path=save_path,
-                              # grad_clip_val=0.5,
-                              grad_clip_val=None,
-                              seed=seed)
+
+    title = (f"{dist_func.__class__.__name__}\n"
+             f"θ density={theta_density:.2f}, "
+             f"θ slope={theta_slope:.2f}, "
+             f"{'meso' if use_rand_seeds else 'micro'}, "
+             f"t{n_trials}")
+
+    theta_density_hats = None
+    theta_slope_hats = None
+    dist_matrices = []
+    dgm_all = []
+    sgm_all = []
+
+    with tqdm(total=n_trials * n_density * n_slope) as pbar:
+        for idx in range(n_trials):
+            theta_density_hats, theta_slope_hats, dist_matrix, dgm, sgm = (
+                calc_distance_grad_matrices(dist_func,
+                                            synth,
+                                            theta_density=theta_density,
+                                            theta_slope=theta_slope,
+                                            n_density=n_density,
+                                            n_slope=n_slope,
+                                            use_rand_seeds=use_rand_seeds,
+                                            seed=seed + idx,
+                                            pbar=pbar)
+            )
+            dist_matrices.append(dist_matrix)
+            dgm_all.append(dgm)
+            sgm_all.append(sgm)
+
+    dist_matrix = tr.stack(dist_matrices)
+    dgm = tr.stack(dgm_all)
+    sgm = tr.stack(sgm_all)
+    if n_trials > 1:
+        # TODO(cm): variance measurements are currently not comparable across losses?
+        dist_avg_var = dist_matrix.var(dim=0).mean()
+        dgm_avg_var = dgm.var(dim=0).mean()
+        sgm_avg_var = sgm.var(dim=0).mean()
+        log.info(f"dist_avg_var={dist_avg_var:.4f}, "
+                 f" dgm_avg_var={dgm_avg_var:.4f}, "
+                 f" sgm_avg_var={sgm_avg_var:.4f}")
+    dist_matrix = dist_matrix.mean(dim=0)
+    dgm = dgm.mean(dim=0)
+    sgm = sgm.mean(dim=0)
+
+    dist_matrix = dist_matrix / dist_matrix.abs().max()
+    max_grad = max(dgm.abs().max(), sgm.abs().max())
+    log.info(f"max_grad={max_grad:.4f}")
+    dgm /= max_grad
+    sgm /= max_grad
+
+    plot_gradients(theta_density,
+                   theta_slope,
+                   dist_matrix,
+                   theta_density_hats,
+                   theta_slope_hats,
+                   dgm,
+                   sgm,
+                   title=title,
+                   save_path=save_path)
