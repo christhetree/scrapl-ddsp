a9f09fa9aaafed8eae41a14470df0f92e6d99ae0
christhetree <christhetree@gmail.com>
2024-12-22 15:08:12 -0400

    [cm] Loss plotting script

--- scripts/plot_loss.py
+++ scripts/plot_loss.py
@@ -1,10 +1,12 @@
 import logging
 import os
 from collections import defaultdict
+from typing import Dict, Optional
 
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from matplotlib.axes import Subplot
 
 from experiments.paths import OUT_DIR
 
@@ -13,74 +15,160 @@ log = logging.getLogger(__name__)
 log.setLevel(level=os.environ.get("LOGLEVEL", "INFO"))
 
 
-if __name__ == "__main__":
-    # tsv_path = os.path.join(OUT_DIR, "loss.tsv")
-    tsv_path = "/Users/puntland/local_christhetree/aim/scrapl-ddsp/out/out/scrapl_adamw_5e-5_b32__texture_32_32_5_meso.tsv"
-    stage = "train"
-    x_col = "step"
-    y_col = "l1_theta"
-
-    # Load using pandas
+def prepare_tsv_data(
+    tsv_path: str,
+    stage: str,
+    x_col: str,
+    y_col: str,
+    trial_col: str = "seed",
+    allow_var_n: bool = False,
+) -> Dict[str, np.ndarray]:
     df = pd.read_csv(tsv_path, sep="\t", index_col=False)
-    # exit()
 
     # Filter out stage
     df = df[df["stage"] == stage]
+    log.info(f"Number of rows before removing warmup steps: {len(df)}")
+    # Remove sanity check rows
+    df = df[~((df["step"] == 0) & (df["stage"] == "val"))]
+    log.info(f"Number of rows after  removing warmup steps: {len(df)}")
 
-    grouped = df.groupby("seed")
-    # Create list of y_col values for each x_col for each seed
-    data = {name: (group[x_col].values, group[y_col].values) for name, group in grouped}
-    data_dict = defaultdict(list)
-    for seed, (x, y) in data.items():
-        assert len(x) == len(y)
-        for curr_x, curr_y in zip(x, y):
-            data_dict[curr_x].append(curr_y)
-    data = data_dict
+    data = defaultdict(list)
+    grouped = df.groupby(trial_col)
+    for _, group in grouped:
+        x_vals = group[x_col].tolist()
+        y_vals = group[y_col].tolist()
+        if stage == "train":
+            for x_val, y_val in zip(x_vals, y_vals):
+                data[x_val].append(y_val)
+        else:
+            grouped_x = group.groupby(x_col)
+            for x_val, group_x in grouped_x:
+                y_val = group_x[y_col].mean()
+                data[x_val].append(y_val)
+    if stage == "test":
+        assert len(data) == 1
+        # Calc mean, 95% CI, and range
+        y_vals = list(data.values())[0]
+        y_mean = np.mean(y_vals)
+        y_std = np.std(y_vals)
+        y_sem = y_std / np.sqrt(len(y_vals))
+        y_95ci = 1.96 * y_sem
+        y_min = np.min(y_vals)
+        y_max = np.max(y_vals)
+        log.info(
+            f"{y_col} mean: {y_mean:.4f}, 95% CI: {y_95ci:.4f} "
+            f"({y_mean - y_95ci:.4f}, {y_mean + y_95ci:.4f}), "
+            f"range: ({y_min:.4f}, {y_max:.4f}), n: {len(y_vals)}"
+        )
 
-    # data = {
-    #     0.1: [0.2, 0.25, 0.22, 0.21],
-    #     0.2: [0.18, 0.20, 0.19, 0.21],
-    #     0.3: [0.15, 0.14, 0.16, 0.13],
-    #     # Add more timestamps and loss values
-    # }
+    x_vals = []
+    y_means = []
+    y_stds = []
+    y_mins = []
+    y_maxs = []
+    y_95cis = []
+    y_ns = []
+    # We use a for loop to handle jagged data
+    for x_val in sorted(data):
+        x_vals.append(x_val)
+        y_vals = data[x_val]
+        n = len(y_vals)
+        y_mean = np.mean(y_vals)
+        y_std = np.std(y_vals)
+        y_sem = y_std / np.sqrt(n)
+        y_95ci = 1.96 * y_sem
+        y_min = np.min(y_vals)
+        y_max = np.max(y_vals)
+        y_means.append(y_mean)
+        y_stds.append(y_std)
+        y_mins.append(y_min)
+        y_maxs.append(y_max)
+        y_95cis.append(y_95ci)
+        y_ns.append(n)
+    if not allow_var_n:
+        assert len(set(y_ns)) == 1, "Found var no. of trials across different x vals"
+    x_vals = np.array(x_vals)
+    y_means = np.array(y_means)
+    y_95cis = np.array(y_95cis)
+    y_mins = np.array(y_mins)
+    y_maxs = np.array(y_maxs)
+    return {
+        "x_vals": x_vals,
+        "y_means": y_means,
+        "y_95cis": y_95cis,
+        "y_mins": y_mins,
+        "y_maxs": y_maxs,
+    }
 
-    # Extract timestamps and loss values
-    timestamps = np.array(list(data.keys()))
-    loss_values = np.array(list(data.values()))
 
-    # Calculate statistics
-    means = np.mean(loss_values, axis=1)
-    std_devs = np.std(loss_values, axis=1)
-    sem = std_devs / np.sqrt(loss_values.shape[1])  # Standard error of the mean
-    conf_interval = 1.96 * sem  # 95% confidence interval
-    loss_min = np.min(loss_values, axis=1)
-    loss_max = np.max(loss_values, axis=1)
+def plot_xy_vals(
+    ax: Subplot,
+    data: Dict[str, np.ndarray],
+    title: Optional[str] = None,
+    plot_95ci: bool = True,
+    plot_range: bool = True,
+) -> None:
+    x_vals = data["x_vals"]
+    y_means = data["y_means"]
+    y_95cis = data["y_95cis"]
+    y_mins = data["y_mins"]
+    y_maxs = data["y_maxs"]
 
-    # Plot
-    plt.figure(figsize=(10, 6))
-
-    # Main line plot for mean loss
-    plt.plot(timestamps, means, label="Mean Loss", color="blue", lw=2)
-
-    # Shaded region for confidence interval
-    plt.fill_between(
-        timestamps,
-        means - conf_interval,
-        means + conf_interval,
-        color="blue",
-        alpha=0.2,
-        label="95% CI",
-    )
-
-    # Shaded region for range
-    plt.fill_between(
-        timestamps, loss_min, loss_max, color="gray", alpha=0.3, label="Range"
-    )
+    mean_label = "mean"
+    if title is not None:
+        mean_label = f"{title} mean"
+    ax.plot(x_vals, y_means, label=mean_label, lw=2)
+    if plot_95ci:
+        ax.fill_between(
+            x_vals,
+            y_means - y_95cis,
+            y_means + y_95cis,
+            # color="blue",
+            alpha=0.2,
+            # label="95% CI",
+        )
+    if plot_range:
+        ax.fill_between(x_vals, y_mins, y_maxs, color="gray", alpha=0.2)
 
     # Labels and legend
-    plt.xlabel("Timestamp")
-    plt.ylabel("Loss")
-    plt.title("Loss Over Time with 95% CI and Range")
-    plt.legend()
-    plt.grid(True)
+    ax.set_xlabel(f"{x_col}")
+    ax.set_ylabel(f"{y_col}")
+    ax.legend()
+    ax.grid(True)
+
+
+if __name__ == "__main__":
+    tsv_names_and_paths = [
+        (
+            "adam",
+            os.path.join(OUT_DIR, f"scrapl_adamw_1e-5_b32__texture_32_32_5_meso.tsv"),
+        ),
+        (
+            "pwa",
+            os.path.join(OUT_DIR, f"scrapl_pwa_sgd_1e-5_b32__texture_32_32_5_meso.tsv"),
+        ),
+        (
+            "saga",
+            os.path.join(
+                OUT_DIR, f"scrapl_saga_sgd_1e-5_b32__texture_32_32_5_meso.tsv"
+            ),
+        ),
+        (
+            "saga_a0.25",
+            os.path.join(
+                OUT_DIR, f"scrapl_saga_sgd_1e-5_b32_a0.25__texture_32_32_5_meso.tsv"
+            ),
+        ),
+    ]
+    stage = "train"
+    # stage = "val"
+    # stage = "test"
+    x_col = "step"
+    y_col = "l1_theta"
+
+    # Plot
+    fig, ax = plt.subplots(figsize=(10, 6))
+    for name, tsv_path in tsv_names_and_paths:
+        data = prepare_tsv_data(tsv_path, stage, x_col, y_col, allow_var_n=False)
+        plot_xy_vals(ax, data, title=name)
     plt.show()
