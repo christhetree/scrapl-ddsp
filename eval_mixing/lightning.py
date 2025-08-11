0734cd6a9e812970793873f663086474007ebdac
christhetree <christhetree@gmail.com>
2025-08-12 00:52:03 +0100

    [cm] Training working with advanced mixing console

--- eval_mixing/lightning.py
+++ eval_mixing/lightning.py
@@ -8,8 +8,6 @@ import torch as tr
 from torch import Tensor as T
 from torch import nn
 
-from automix.models.dmc import Mixer
-
 logging.basicConfig()
 log = logging.getLogger(__name__)
 log.setLevel(level=os.environ.get("LOGLEVEL", "INFO"))
@@ -21,28 +19,48 @@ class MixingLightingModule(pl.LightningModule):
         encoder: nn.Module,
         emb_dim: int,
         mixer: nn.Module,
+        n_track_params: int,
+        n_fx_bus_params: int,
+        n_master_bus_params: int,
         loss_func: nn.Module,
         pp_hidden_dim: int = 256,
         pp_dropout: float = 0.2,
     ):
         super().__init__()
+        assert n_track_params > 0, "n_track_params must be greater than 0"
         self.encoder = encoder
         self.emb_dim = emb_dim
         self.mixer = mixer
+        self.n_track_params = n_track_params
+        self.n_fx_bus_params = n_fx_bus_params
+        self.n_master_bus_params = n_master_bus_params
         self.loss_func = loss_func
         self.pp_hidden_dim = pp_hidden_dim
         self.pp_dropout = pp_dropout
 
-        self.post_processor = nn.Sequential(
+        self.n_global_params = n_fx_bus_params + n_master_bus_params
+        self.track_post_processor = nn.Sequential(
             tr.nn.Linear(2 * emb_dim, pp_hidden_dim),
             tr.nn.Dropout(pp_dropout),
             tr.nn.PReLU(),
             tr.nn.Linear(pp_hidden_dim, pp_hidden_dim),
             tr.nn.Dropout(pp_dropout),
             tr.nn.PReLU(),
-            tr.nn.Linear(pp_hidden_dim, self.mixer.num_params),
+            tr.nn.Linear(pp_hidden_dim, n_track_params),
             tr.nn.Sigmoid(),
         )
+        self.global_post_processor = None
+        if self.n_global_params > 0:
+            self.global_post_processor = nn.Sequential(
+                tr.nn.Linear(emb_dim, pp_hidden_dim),
+                tr.nn.Dropout(pp_dropout),
+                tr.nn.PReLU(),
+                tr.nn.Linear(pp_hidden_dim, pp_hidden_dim),
+                tr.nn.Dropout(pp_dropout),
+                tr.nn.PReLU(),
+                tr.nn.Linear(pp_hidden_dim, self.n_global_params),
+                tr.nn.Sigmoid(),
+            )
         self.metrics = nn.ModuleDict(
             {
                 "mss": auraloss.freq.MultiResolutionSTFTLoss(),
@@ -74,38 +92,56 @@ class MixingLightingModule(pl.LightningModule):
         track_emb = self.encoder(x)
         track_emb = track_emb.view(bs, num_tracks, -1)  # (bs, num_tracks, d_embed)
 
-        # Generate the "context" embedding
-        context_emb = []
-        for bidx in range(bs):
-            c_n = track_emb[bidx, ~track_mask[bidx, :], :].mean(
-                dim=0, keepdim=True
-            )  # (bs, 1, d_embed)
-            c_n = c_n.repeat(num_tracks, 1)  # (bs, num_tracks, d_embed)
-            context_emb.append(c_n)
-        context_emb = tr.stack(context_emb, dim=0)
+        # Move tracks back from batch dim
+        x = x.view(bs, num_tracks, -1)
+
+        # # Generate the "context" embedding
+        # context_emb = []
+        # for bidx in range(bs):
+        #     c_n = track_emb[bidx, ~track_mask[bidx, :], :].mean(
+        #         dim=0, keepdim=True
+        #     )  # (bs, 1, d_embed)
+        #     c_n = c_n.repeat(num_tracks, 1)  # (bs, num_tracks, d_embed)
+        #     context_emb.append(c_n)
+        # context_emb = tr.stack(context_emb, dim=0)
 
         # ==============================================================================
-        # # Compute sum over non-masked tracks
-        # sum_emb = (track_emb * ~track_mask.unsqueeze(-1)).sum(dim=1)  # (bs, d_embed)
-        # count = (~track_mask).sum(dim=1, keepdim=True)  # (bs, 1)
-        #
-        # # Mean embedding per batch
-        # mean_emb = sum_emb / count  # (bs, d_embed)
-        #
-        # # Expand to (bs, num_tracks, d_embed)
-        # context_emb2 = mean_emb.unsqueeze(1).expand(-1, num_tracks, -1)
+        # Compute sum over non-masked tracks
+        sum_emb = (track_emb * ~track_mask.unsqueeze(-1)).sum(dim=1)  # (bs, d_embed)
+        count = (~track_mask).sum(dim=1, keepdim=True)  # (bs, 1)
+
+        # Mean embedding per batch
+        mean_emb = sum_emb / count  # (bs, d_embed)
+
+        # Expand to (bs, num_tracks, d_embed)
+        context_emb = mean_emb
         # assert tr.allclose(context_emb, context_emb2)
         # ==============================================================================
 
         # Fuse the track embs and context embs
-        emb = tr.cat((track_emb, context_emb), dim=-1)  # (bs, num_tracks, 2 * d_embed)
+        context_emb_expanded = context_emb.unsqueeze(1).expand(-1, num_tracks, -1)
+        fused_emb = tr.cat(
+            (track_emb, context_emb_expanded), dim=-1
+        )  # (bs, num_tracks, 2 * d_embed)
 
         # Estimate mixing parameters for each track (in parallel)
-        p_hat_0to1 = self.post_processor(emb)  # (bs, num_tracks, num_params)
-
-        # Generate the stereo mix
-        x = x.view(bs, num_tracks, -1)  # move tracks back from batch dim
-        y_hat, p_hat = self.mixer(x, p_hat_0to1)  # (bs, 2, n_samples) # and denormalized params
+        p_track_hat_0to1 = self.track_post_processor(
+            fused_emb
+        )  # (bs, num_tracks, num_params)
+        if self.n_global_params > 0:
+            p_global_hat_0to1 = self.global_post_processor(context_emb)
+            p_fx_bus_hat_0to1 = p_global_hat_0to1[:, : self.n_fx_bus_params]
+            p_master_bus_hat_0to1 = p_global_hat_0to1[:, self.n_fx_bus_params :]
+            y_track_hat, y_hat, p_track_hat, p_fx_hat, p_master_hat = self.mixer(
+                tracks=x,
+                track_params=p_track_hat_0to1,
+                fx_bus_params=p_fx_bus_hat_0to1,
+                master_bus_params=p_master_bus_hat_0to1,
+            )
+        else:
+            y_hat, p_hat = self.mixer(
+                x, p_track_hat_0to1
+            )  # (bs, 2, n_samples) # and denormalized params
 
         # Compute loss
         loss = self.loss_func(y_hat, y)
